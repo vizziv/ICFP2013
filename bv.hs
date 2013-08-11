75911f61d759d38a12b5b3406194eaeaab19c7bc
Ziv Scully <zivscully@gmail.com>
2013-08-11 00:10:33 -0400

    Haskell \BV implemented more verbosely :(.
    However, this might help manipulation of programs, which is kind of important... maybe?

--- bv.hs
+++ bv.hs
@@ -1,23 +1,22 @@
 import Control.Monad
 import Control.Monad.Reader
 import Data.Bits
+import Data.Char (toLower)
 import Data.Maybe (fromJust)
 import Data.Word (Word64)
 import Text.Printf (printf)
 
 data Program = Program Identifier Expression
 data Expression = Zero | One
-                | Variable Identifier
+                | Var Identifier
                 | If Expression Expression Expression
                 | Fold Expression Expression Identifier Identifier Expression
-                | UnaryForm UnaryOp Expression
-                | BinaryForm BinaryOp Expression Expression
-
-data UnaryOp = UnaryOp String (Word64 -> Word64)
-data BinaryOp = BinaryOp String (Word64 -> Word64 -> Word64)
-
+                | UF UnaryOp Expression -- unary form
+                | BF BinaryOp Expression Expression -- binary form
+data UnaryOp = Not | Shl1 | Shr1 | Shr4 | Shr16 deriving (Eq, Show)
+data BinaryOp = And | Or | Xor | Plus deriving (Eq, Show)
 -- We never need more than 3 variables, and ziv likes brevity....
-data Identifier = X | Y | Z deriving (Eq, Show)
+data Identifier = Input | Byte | Acc deriving Eq
 
 instance Show Program where
   show (Program param body) =
@@ -26,7 +25,7 @@ instance Show Program where
 instance Show Expression where
   show Zero = "0"
   show One  = "1"
-  show (Variable name) = show name
+  show (Var name) = show name
 
   show (If cond zero nonzero) =
     printf "(if0 %s %s %s)" (show cond) (show zero) (show nonzero)
@@ -34,17 +33,16 @@ instance Show Expression where
   show (Fold bytes initial param1 param2 body) =
     printf "(fold %s %s (lambda (%s %s) %s))" (show bytes) (show initial) (show param1) (show param2) (show body)
 
-  show (UnaryForm op arg) =
-    printf "(%s %s)" (show op) (show arg)
+  show (UF op arg) =
+    printf "(%s %s)" (map toLower $ show op) (show arg)
 
-  show (BinaryForm op arg1 arg2) =
-    printf "(%s %s %s)" (show op) (show arg1) (show arg2)
+  show (BF op arg1 arg2) =
+    printf "(%s %s %s)" (map toLower $ show op) (show arg1) (show arg2)
 
-instance Show UnaryOp where
-  show (UnaryOp name _) = name
-
-instance Show BinaryOp where
-  show (BinaryOp name _) = name
+instance Show Identifier where
+  show Input = "input"
+  show Byte = "byte"
+  show Acc = "acc"
 
 psize :: Program -> Int
 psize (Program param body) = 1 + size body
@@ -52,11 +50,11 @@ psize (Program param body) = 1 + size body
 size :: Expression -> Int
 size Zero = 1
 size One = 1
-size (Variable _) = 1
+size (Var _) = 1
 size (If cond zero nonzero) = 1 + size cond + size zero + size nonzero
 size (Fold bytes initial param1 param2 body) = 2 + size bytes + size initial + size body
-size (UnaryForm op arg) = 1 + size arg
-size (BinaryForm op arg1 arg2) = 1 + size arg1 + size arg2
+size (UF op arg) = 1 + size arg
+size (BF op arg1 arg2) = 1 + size arg1 + size arg2
 
 -- Because we only ever have 3 variables, a list is fine.
 type Context a = Reader [(Identifier,Word64)] a
@@ -80,7 +78,7 @@ eval (Program param body) value = flip runReader [] $ putVar param value $ (eeva
 eeval :: Expression -> Context Word64
 eeval Zero = return 0
 eeval One = return 1
-eeval (Variable x) = getVar x
+eeval (Var x) = getVar x
 eeval (If cond zero nonzero) = do
   c <- eeval cond
   eeval $ if c == 0 then zero else nonzero
@@ -92,24 +90,30 @@ eeval (Fold bytes initial param1 param2 body) = do
   -- That is, the accumulator (param2) is on the left, and the current byte (param1) is on the right.
   let op acc b = putVar param2 acc . putVar param1 b $ eeval body
   foldM op i . toBytes $ bs
-eeval (UnaryForm (UnaryOp _ op) arg) = liftM op (eeval arg)
-eeval (BinaryForm (BinaryOp _ op) arg1 arg2) = liftM2 op (eeval arg1) (eeval arg2)
+eeval (UF op arg) = liftM (applyUnary op) (eeval arg)
+eeval (BF op arg1 arg2) = liftM2 (applyBinary op) (eeval arg1) (eeval arg2)
 
-bnot = UnaryForm $ UnaryOp "shl1" $ complement
-shl1 = UnaryForm $ UnaryOp "shl1" $ flip shiftL 1
-shr1 = UnaryForm $ UnaryOp "shr1" $ flip shiftR 1
-shr4 = UnaryForm $ UnaryOp "shr4" $ flip shiftR 4
-shr16 = UnaryForm $ UnaryOp "shr16" $ flip shiftR 16
-band = BinaryForm $ BinaryOp "and" (.&.)
-bor = BinaryForm $ BinaryOp "or" (.|.)
-bxor = BinaryForm $ BinaryOp "xor" xor
-plus = BinaryForm $ BinaryOp "plus" (+)
-x = Variable X
-y = Variable Y
-z = Variable Z
+applyUnary :: UnaryOp -> Word64 -> Word64
+applyUnary Not = complement
+applyUnary Shl1 = flip shiftL 1
+applyUnary Shr1 = flip shiftR 1
+applyUnary Shr4 = flip shiftR 4
+applyUnary Shr16 = flip shiftR 16
+
+applyBinary :: BinaryOp -> Word64 -> Word64 -> Word64
+applyBinary And = (.&.)
+applyBinary Or = (.|.)
+applyBinary Xor = xor
+applyBinary Plus = (+)
+
+-- Aliases because ziv is lazy.
+program = Program Input
+fold bytes initial = Fold bytes initial Byte Acc
+tfold = program . fold (Var Input) Zero
 
 -- Reverses the bytes of the input.
 -- The program is equivalent to foldl (\xs x -> xs*256 + x) 0 . toBytes
-main = print $ eval (Program X $ Fold x Zero Y Z
-                     (plus y $ shl1 . shl1 . shl1 . shl1 . shl1 . shl1 . shl1 . shl1 $ z)
-                    ) $ 2553
+main = print $ eval (tfold
+                     (BF Plus (Var Byte)
+                      (UF Shl1 . UF Shl1 . UF Shl1 . UF Shl1 . UF Shl1 . UF Shl1 . UF Shl1 . UF Shl1 $
+                       (Var Acc)))) 2553
